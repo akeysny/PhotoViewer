c4c9eee96e0ea768efd0367878f5dbd7c70aaf42
Anna Taylor <annatnyc@gmail.com>
2021-02-12 23:02:53 -0500

    Initial Commit

---
+++ Shared/PhotoViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoViewerApp.swift
+//  Shared
+//
+//  Created by Anna Taylor on 2/12/21.
+//
+
+import SwiftUI
+
+@main
+struct PhotoViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
